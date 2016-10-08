1c536000f3e90d634fd5daed9f096ec9d7ce7af7
echosoar <82163514@qq.com>
2016-10-08 23:45:13 +0800

    update@161008

--- api/deleteHost.js
+++ api/deleteHost.js
@@ -4,6 +4,37 @@ const fs = require("fs");
 
 const hostWINXP = "C:/Windows/System32/drivers/etc/hosts";
 
+let deleteFolderRecursive = function(path) {
+
+    var files = [];
+
+    if( fs.existsSync(path) ) {
+
+        files = fs.readdirSync(path);
+
+        files.forEach(function(file,index){
+
+            var curPath = path + "/" + file;
+
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+
+                deleteFolderRecursive(curPath);
+
+            } else { // delete file
+
+                fs.unlinkSync(curPath);
+
+            }
+
+        });
+
+        fs.rmdirSync(path);
+
+    }
+
+};
+
+
 let main = function(obj){
 	
 	if(fs.existsSync(hostWINXP)){
@@ -28,6 +59,8 @@ let main = function(obj){
 			}else if(nomoHostBool){
 				let temHost = lineArr[i].split(/\s/);
 				if(temHost[1]==nowHost){
+					var dirname = __dirname+"/../data/"+(new Buffer(nowHost)).toString('base64');
+					deleteFolderRecursive(dirname)
 					continue;
 				}
 			}
