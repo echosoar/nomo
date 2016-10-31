d1605f84716ef89e179cfc641323bac679057215
echosoar <82163514@qq.com>
2016-10-31 23:20:53 +0800

    update@161031

--- api/hostToIp.js
+++ api/hostToIp.js
@@ -3,11 +3,11 @@ const child_process = require('child_process')
 let hostToIp = host => {
 	if(/^(?:[\w][\w-]*[\w]\.|[\w]\.)+(?:[\w]{2,})$/.test(host)){
 		try {
-			let pingRes = child_process.execSync("ping "+host+" -l 0 -n 1").toString();
+			let pingRes = child_process.execSync("ping "+host+" -l 0 -n 1", {stdio:[0,'']}).toString();
 			return  /(\d{1,3}\.){3}\d{1,3}/.exec(pingRes)[0];
 		} catch(e) {
 			try {
-				let pingRes = child_process.execSync("ping -c 1 -s 0 "+host).toString(); // For OSX
+				let pingRes = child_process.execSync("ping -c 1 -s 0 "+host, {stdio:[0,'']}).toString(); // For OSX
 				return  /(\d{1,3}\.){3}\d{1,3}/.exec(pingRes)[0];
 			} catch(e) {
 				return false;
