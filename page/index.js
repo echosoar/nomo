99b211058bdd6ca57598e0adfbefd11ad4c312e1
echosoar <82163514@qq.com>
2016-11-22 23:55:39 +0800

    update@161122 reedit pages

--- page/index.js
+++ page/index.js
@@ -3,8 +3,29 @@
 const fs = require("fs");
 
 let index = function * (next){
-	this.response.type = 'text/html; charset=utf-8';
-	this.body = fs.readFileSync(__dirname + "/index.html");
+
+	let url = this.request.url;
+	if(url=="/")url="/index.html";
+	
+	let ext = url.split(".").pop();
+	
+	let contentType = {
+		"html":"text/html",
+		"css":"text/css",
+		"js":"application/x-javascript",
+		"png":"image/png",
+		"gif":"image/gif"
+	}
+	
+	let fileType = contentType[ext] || "text/plain";
+	
+	this.response.type = fileType + '; charset=utf-8';
+	
+	let fileAddr = __dirname + url;
+	
+	if(fs.existsSync(fileAddr)) this.body = fs.readFileSync(fileAddr);
+	else this.body = '404 Nomo Not Found!';
+	
 	yield next;
 }
 
