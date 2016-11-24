d7da4305fc3eb0934ad60503b81c4206d7050270
echosoar <82163514@qq.com>
2016-11-25 00:02:44 +0800

    update@161125 new function at add new api

--- api/addNewApi.js
+++ api/addNewApi.js
@@ -6,10 +6,24 @@ const fs = require("fs");
 let addNewApi = obj => {
 	var $_GET = /\?host=(.*?)&https=(.*?)&api=(.*?)$/.exec(obj.url);
 	var nowHost = $_GET[1];
+	var isHttps = $_GET[2]=="true";
+	var api = $_GET[3];
+	var apiName = (new Buffer(api)).toString('base64');
 	var dirname = path.resolve(__dirname, "../data/"+(new Buffer(nowHost)).toString('base64'));
 	var configFile = dirname+"/config.json";
+	var apiFile = dirname+"/"+apiName+".json";
 	var config = JSON.parse(fs.readFileSync(configFile));
-	
+	var addRes = false;
+	if(!config.api[apiName]){
+		config.api[apiName] = {
+			name:api,
+			isHttps:isHttps
+		}
+		addRes = true;
+		fs.writeFileSync(apiFile,"");
+		fs.writeFileSync(configFile,JSON.stringify(config));
+	}
+	obj.body = JSON.stringify({res:addRes?1:0});
 }
 
 module.exports = addNewApi;
